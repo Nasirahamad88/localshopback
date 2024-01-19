32397d4613e8aa2f9c6d35b47b2b1c23966075bc
Nasirahamad88 <onlineworkbd88888@gmail.com>
2024-01-19 21:30:25 +0600

    first commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,2 @@
-export const schemaTypes = []
+import Products from "./products"
+export const schemaTypes = [Products]
---
+++ schemas/products.js
@@ -0,0 +1,40 @@
+
+
+export default ({
+  name: 'product',
+  title: 'Product',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+      required: true,
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      },
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    },
+    // Add more fields as needed (categories, tags, inventory, etc.)
+  ],
+});
